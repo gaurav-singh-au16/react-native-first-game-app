31f315f99410f7c9731cb60c70d2896e83babb05
Gaurav Singh <74302871+gaurav-singh-au16@users.noreply.github.com>
2021-12-30 23:52:28 +0530

    react-native-first-game-app

---
+++ App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './src/HomeScreen'
+
+
+export default function App() {
+
+  
+  return (
+
+    <View style={styles.container}>
+     
+      <HomeScreen />
+      <StatusBar style="auto" />
+    </View>
+
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#223344',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
